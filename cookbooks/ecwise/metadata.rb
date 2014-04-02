e85566f3ac7467580fe0480b0bc4125408999216
ssmiley <ssmiley@ecwise.com>
2014-04-02 21:47:03 +0000

    Added rightscale_marker to default.rb

--- cookbooks/ecwise/metadata.rb
+++ cookbooks/ecwise/metadata.rb
@@ -4,5 +4,9 @@ maintainer_email 'YOUR_EMAIL'
 license          'All rights reserved'
 description      'Installs/Configures ecwise'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+
 version          '0.1.0'
+
+depends "rightscale"
+
 recipe "ecwise::default","Prints hello world output"
